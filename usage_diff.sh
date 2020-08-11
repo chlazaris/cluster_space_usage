9e20f9ed170afca650421d02fff463c70fd19740
lazaris <lazaris@tak.wi.mit.edu>
2020-08-11 14:49:24 -0400

    Calculate the difference of space used per user between two different dates

--- usage_diff.sh
+++ usage_diff.sh
@@ -9,7 +9,6 @@ dir2=$2
 # Create a directory to save the difference
 outdir=${dir1}_vs_${dir2}
 mkdir $outdir
-cd $outdir
 
 # Create the file with the usage difference
-join -1 1 -2 1 <(sort $dir1/ordered_usage_per_user.txt) <(sort $dir2/ordered_usage_per_user.txt) | awk '{print $0,$2-$3}' | awk '{if ($4!=0) {print $0}}' | sort -k4nr > usage_difference.txt
+join -1 1 -2 1 <(sort $dir1/ordered_usage_per_user.txt) <(sort $dir2/ordered_usage_per_user.txt) | awk '{print $0,$2-$3}' | awk '{if ($4!=0) {print $0}}' | sort -k4nr > $outdir/usage_difference.txt
